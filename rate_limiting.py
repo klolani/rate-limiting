e8c86ea6872eb520a550019063b2d91d8a70e11d
kenn <klolani@yahoo.com>
2020-04-15 23:50:37 +0300

    Refactor rate Limiter to run tests

--- rate_limiting.py
+++ rate_limiting.py
@@ -2,98 +2,100 @@ import json
 import time
 import redis
 
-from flask import g, request
-
 def get_identifiers():
     ret = ['ip:' + request.remote_addr]
     if g.user.is_authenticated():
         ret.append('user:' + g.user.get_id())
     return ret
 
-def sliding_window(conn, weight=1, limits=[(1, 10), (60, 120), (3600, 240, 60)], redis_time=False):
-    if not hasattr(conn, 'sliding_window_lua'):
-        conn.sliding_window_lua = conn.register_script(sliding_window_lua_)
+def sliding_window(conn, base_keys, minute=0, hour=0, day=0, weight=1):
+    limits = [minute, hour, day, weight, int(time.time())]
+    return bool(sliding_window_lua(conn, keys=base_keys, args=limits))
 
-    now = conn.time()[0] if redis_time else time.time()
-    return conn.sliding_window_lua(
-        keys=get_identifiers(), args=[json.dumps(limits), now, weight])
+def _script_load(script):
+    sha = [None]
+    def call(conn, keys=[], args=[], force_eval=False):
+        if not force_eval:
+            if not sha[0]:
+                sha[0] = conn.execute_command(
+                    "SCRIPT", "LOAD", script, parse="LOAD")
+            try:
+                return conn.execute_command(
+                    "EVALSHA", sha[0], len(keys), *(keys+args))
+            except redis.exceptions.ResponseError as msg:
+                if not msg.args[0].startswith("NOSCRIPT"):
+                    raise
+        return conn.execute_command(
+            "EVAL", script, len(keys), *(keys+args))
+    return call
 
-sliding_window_lua_ = '''
-local limits = cjson.decode(ARGV[1])
-local now = tonumber(ARGV[2])
-local weight = tonumber(ARGV[3] or '1')
-local longest_duration = limits[1][1] or 0
-local saved_keys = {}
+sliding_window_lua = _script_load('''
+local slice = {60, 3600, 86400}
+local precision = {1, 60, 1800}
+local dkeys = {'m', 'h', 'd'}
+local ts = tonumber(table.remove(ARGV))
+local weight = tonumber(table.remove(ARGV))
+local fail = false
 
-for i, limit in ipairs(limits) do
-    local duration = limit[1]
-    longest_duration = math.max(longest_duration, duration)
-    local precision = limit[3] or duration
-    precision = math.min(precision, duration)
-    local blocks = math.ceil(duration / precision)
-    local saved = {}
-    table.insert(saved_keys, saved)
-    saved.block_id = math.floor(now / precision)
-    saved.trim_before = saved.block_id - blocks + 1
-    saved.count_key = duration .. ':' .. precision .. ':'
-    saved.ts_key = saved.count_key .. 'o'
-    for j, key in ipairs(KEYS) do
-        local old_ts = redis.call('HGET', key, saved.ts_key)
-        old_ts = old_ts and tonumber(old_ts) or saved.trim_before
-        if old_ts > now then
+for _, ready in ipairs({false, true}) do
 
-            return 1
-        end
+    for i = 1, math.min(#ARGV, #slice) do
+        local limit = tonumber(ARGV[i])
 
-        local decr = 0
-        local dele = {}
-        local trim = math.min(saved.trim_before, old_ts + blocks)
-        for old_block = old_ts, trim - 1 do
-            local bkey = saved.count_key .. old_block
-            local bcount = redis.call('HGET', key, bkey)
-            if bcount then
-                decr = decr + tonumber(bcount)
-                table.insert(dele, bkey)
+        if limit > 0 then
+
+            local cutoff = ts - slice[i]
+            local curr = '' .. (precision[i] * math.floor(ts / precision[i]))
+            local suff = ':' .. dkeys[i]
+            local suff2 = suff .. ':l'
+
+            for j, k in ipairs(KEYS) do
+                local key = k .. suff
+                local key2 = k .. suff2
+                if ready then
+                    -- if we get here, our limits are fine
+                    redis.call('incrby', key, weight)
+                    local oldest = redis.call('lrange', key2, '0', '1')
+                    if oldest[2] == curr then
+                        redis.call('ltrim', key2, 0, -3)
+                        redis.call('rpush', key2, weight + tonumber(oldest[1]), oldest[2])
+                    else
+                        redis.call('rpush', key2, weight, curr)
+                    end
+                    redis.call('expire', key, slice[i])
+                    redis.call('expire', key2, slice[i])
+                else
+
+                    local total = tonumber(redis.call('get', key) or '0')
+
+                    while total + weight > limit do
+                        local oldest = redis.call('lrange', key2, '0', '1')
+                        if #oldest == 0 then
+                            break
+                        end
+                        if tonumber(oldest[2]) <= cutoff then
+                            total = tonumber(redis.call('incrby', key, -tonumber(oldest[1])))
+                            redis.call('ltrim', key2, '2', '-1')
+                        else
+                            break
+                        end
+                    end
+                    fail = fail or total + weight > limit
+                end
             end
         end
-
-        local cur
-        if #dele > 0 then
-            redis.call('HDEL', key, unpack(dele))
-            cur = redis.call('HINCRBY', key, saved.count_key, -decr)
-        else
-            cur = redis.call('HGET', key, saved.count_key)
-        end
-
-        if tonumber(cur or '0') + weight > limit[2] then
-            return 1
-        end
+    end
+    if fail then
+        break
     end
 end
+return fail
+''')
 
-for i, limit in ipairs(limits) do
-    local saved = saved_keys[i]
-    for j, key in ipairs(KEYS) do
-        -- update the current timestamp, count, and bucket count
-        redis.call('HSET', key, saved.ts_key, saved.trim_before)
-        redis.call('HINCRBY', key, saved.count_key, weight)
-        redis.call('HINCRBY', key, saved.count_key .. saved.block_id, weight)
-    end
-end
-
-if longest_duration > 0 then
-    for _, key in ipairs(KEYS) do
-        redis.call('EXPIRE', key, longest_duration)
-    end
-end
-return 0
-'''
-
-def test(count=200000):
+def test(count=10000):
     import uuid
     keys = [str(uuid.uuid4())]
-    c = redis.Redis()
-
+    c = redis.Redis(host='redis', port=6379)
 
     t = time.time()
     for i in range(count):
